53098645a160c7994f2e657f6430fbab56789d63
Saddam Hossen <saddam.itsd@goldenharvestbd.com>
2021-04-25 12:21:02 +0600

    first commit

---
+++ Domain/Entities/DepartmentType.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class DepartmentType : AuditableBaseEntity
+    {
+        [Key]
+        public int DepartmentTypeId { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string DepartmentName { get; set; }
+    }
+}
